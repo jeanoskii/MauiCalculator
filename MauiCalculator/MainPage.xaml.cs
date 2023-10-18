f07f0d0d75df8b2c3e4e1bf3cb34a9e553b7d767
unknown <thebeangodzilla22@gmail.com>
2023-10-18 22:07:29 +0800

    Fixed formatting, added methods to simplify code

--- MauiCalculator/MainPage.xaml.cs
+++ MauiCalculator/MainPage.xaml.cs
@@ -2,67 +2,95 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	//int count = 0; Unnecessary
+
+    enum Operator
+    {
+        Addition,
+        Multiplication,
+        Division,
+        Subtraction,
+        Exponent
+    }
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    //private void OnCounterClicked(object sender, EventArgs e)
+    //{
+    //	count++;
+    //} Unnecessary
 
-	}
+    private double calculate(double[] operands, Operator op) {
+        switch (op)
+        {
+            case Operator.Addition:
+                return operands[0] + operands[2];
+        }
+        return 0;
+    }
+
 
     private void btnBackSpace_Clicked(object sender, EventArgs e)
     {
-		lblOutput.Text = "";
+		lblInputOutput.Text = "0";
+    }
+
+    private void addInputOutputText(char input)
+    {
+      lblInputOutput.Text = (lblInputOutput.Text == "0") ? input.ToString() : lblInputOutput.Text += input;
+    }
+
+    private void btn0_clicked(object sender, EventArgs e)
+    {
+        addInputOutputText('0');
     }
 
     private void btn1_Clicked(object sender, EventArgs e)
     {
-		lblOutput.Text = lblOutput.Text + "1";
+        addInputOutputText('1');
     }
 
     private void btn2_Clicked(object sender, EventArgs e)
     {
-		lblOutput.Text = lblOutput.Text + "2";
+        addInputOutputText('2');
     }
 
     private void btn3_Clicked(object sender, EventArgs e)
     {
-        lblOutput.Text = lblOutput.Text + "3";
+        addInputOutputText('3');
     }
 
     private void btn4_Clicked(object sender, EventArgs e)
     {
-        lblOutput.Text = lblOutput.Text + "4";
+        addInputOutputText('4');
     }
 
     private void btn5_Clicked(object sender, EventArgs e)
     {
-        lblOutput.Text = lblOutput.Text + "5";
+        addInputOutputText('5');
     }
 
     private void btn6_Clicked(object sender, EventArgs e)
     {
-        lblOutput.Text = lblOutput.Text + "6";
+        addInputOutputText('6');
     }
 
     private void btn7_Clicked(object sender, EventArgs e)
     {
-        lblOutput.Text = lblOutput.Text + "7";
+        addInputOutputText('7');
     }
 
     private void btn8_Clicked(object sender, EventArgs e)
     {
-        lblOutput.Text = lblOutput.Text + "8";
+        addInputOutputText('8');
     }
 
     private void btn9_Clicked(object sender, EventArgs e)
     {
-        lblOutput.Text = lblOutput.Text + "9";
+        addInputOutputText('9');
     }
 }
 
